c27fc3af9103fe1a35103eac032a0bdd5b8e5014
bielro69 <00001110481494sp@al.educacao.sp.gov.br>
2024-11-01 13:36:16 -0300

    flashcardd

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'neymar',
+    'em que ano neymar ficou top 10 melhores do mundo jogando pelo santos?',
+    'em 2012'
+)
+
+criaCartao(
+    'neymar',
+    'quantos anos neymar ficou na frança?',
+    'ele ficou 5 anos'
+)
+
+criaCartao(
+    'neymar',
+    'quantas bolas de ouro o neymar tem ?',
+    'por incrivel que pareça o neymar não tem nenhuma bola de ouro'
+)
+
+criaCartao(
+    'neymar',
+    'quantos filhos neymar tem ?',
+    'ele tem 3 filhos'
+)
